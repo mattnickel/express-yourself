0c8cc432ece9b92a9bb34397ce1e34aefbd03edc
Matt Nickel <matt@nickelimages.com>
2015-07-08 12:53:20 -0700

    Added CRUD

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var express = require('express');
 var path = require('path');
 var app = express();
 var port = 3000;
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, '/public')));
 //view setup
@@ -12,12 +14,26 @@ app.set('views', path.join(__dirname, 'views'));
 //view engine setup
 app.set('view engine', 'jade');
 
-//get request
-app.get('/', function(req,res){
-  res.render('index');
+app.post('/index', function(req,res){
+  console.log(req.body);
+  res.json(req.body);
 });
 
-//app listener
+app.get('/index',function(req,res){
+  var title = ["one","two"];
+  res.json(title);
+  console.log(title);
+});
+app.delete('/index',function(req,res){
+  req.body.remove;
+  res.json("That sucker has been destroyed");
+});
+app.put('/index',function(req,res){
+  req.body.find;
+  req.body.remove;
+  req.body.save;
+  res.json("Changed");
+});
 app.listen(port, function(){
   console.log('server running on ' + port);
 });
