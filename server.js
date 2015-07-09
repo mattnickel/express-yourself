0e1ce7d1b3ce8d44e315d702c55c8a6936d90e4b
Matt Nickel <matt@nickelimages.com>
2015-07-09 10:16:24 -0700

    Set up db

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 var express = require('express');
 var path = require('path');
 var app = express();
+var mongoose = require('mongoose')
 var port = 3000;
 
 app.use(express.static(path.join(__dirname, '/public')));
