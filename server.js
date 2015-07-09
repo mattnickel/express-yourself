bbcddcb64c1473fb5eaa28f3b077529889343ded
Matt Nickel <matt@nickelimages.com>
2015-07-09 13:07:30 -0700

    MongoDb

--- server.js
+++ server.js
@@ -3,13 +3,58 @@
 var express = require('express');
 var path = require('path');
 var app = express();
-var mongoose = require('mongoose')
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 var port = 3000;
 
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/blogs')
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/public')));
 //view setup
 app.set('views', path.join(__dirname, 'views'));
 
+
+var articleSchema= mongoose.Schema ({
+  title: String,
+  blog: String
+});
+
+var article = mongoose.model('article',articleSchema);
+
+//article get & post & delete
+app.get('/articles', function(req,res){
+  article.find(function(err,article) {
+    res.json(article);
+  });
+});
+
+app.post('/articles', function(req,res){
+  console.log("Inside Post");
+  console.log(req.body);
+  var postArticleData = JSON.parse(JSON.stringify(req.body));
+  var postArticle = new article(postArticleData);
+  postArticle.save(function(err,postArticle){
+    if (err){
+      console.log("Bummer")
+    }else {
+      console.log("Cool");
+      res.json(postArticle);
+    }
+  });
+
+  article.find(function(err,article) {
+    res.json(article);
+  });
+});
+
+app.delete('/articles:article_id', function(req,res){
+  article.remove({
+    _id : req.params.article_id
+  });
+  article.find(function(err,article) {
+    res.json(article);
+  });
+});
 //view engine setup
 app.set('view engine', 'jade');
 
